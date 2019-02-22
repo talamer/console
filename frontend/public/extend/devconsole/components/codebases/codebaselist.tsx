2c32ccdb90e69529ea2ba2b470340f5578ac72b7
root <root@localhost.localdomain>
2019-02-22 19:28:35 +0530

    feat(codebases):Add codebases section to devOps console

--- frontend/public/extend/devconsole/components/codebases/codebaselist.tsx
+++ frontend/public/extend/devconsole/components/codebases/codebaselist.tsx
@@ -1,21 +1,42 @@
 import * as React from 'react';
+import { ResourceIcon } from '../../../../components/utils';
 
 export class CodeBaseList extends React.Component<codebaseListProps> {
   constructor(props) {
     super(props);
+    this.updateDisplay();
+    console.log(this.display);
   }
-  componentDidUpdate(){
-      this.updateDisplay();
-  }
+  display:Object = <h3 className = 'noCodebasesFound'> No codebases found. </h3>; 
   updateDisplay = () => {
     var list = this.props.list;
     if(list && list.length == 0){
-        this.display = <h3 className = 'noCodebasesFound'> No codebases found. </h3>;
+        this.display = <h3 className = 'noCodebasesFound co-text-service'> No codebases found. </h3>;
     }
-    else 
-    this.display = <h3 className = 'noCodebasesFound'> /loading from namespace. </h3>;
+    else{ 
+        console.log("else",list,list.length)
+        this.display = list.map((item,key)=>{
+            return ( 
+                <div className="co-ip-row" key={key}>
+                <div className="row">
+                  <div className="col-lg-2 col-md-2 col-sm-4 col-xs-6 co-text-service">
+                    <p>{item.name || '-'}</p>
+                  </div>
+                  <div className="col-lg-2 col-md-2 col-sm-4 col-xs-6 co-text-service">
+                    <p className="co-text-node"><ResourceIcon kind="Node" /><span>{item.namespace}</span></p>
+                  </div>
+                  <div className="col-lg-5 col-md-5 col-sm-4 hidden-xs co-text-service">
+                    <p>{item.description}</p>
+                  </div>
+                  <div className="col-lg-3 col-md-3 hidden-sm hidden-xs co-text-pod">
+                    <p><ResourceIcon kind="Pod" /><span>{item.location}</span></p>
+                  </div>
+                </div>
+              </div>
+            )
+            });    
+    }
   }
-  display = <h3 className = 'noCodebasesFound'> No codebases found. </h3>; 
   render() {
     return(
       <React.Fragment>
@@ -28,5 +49,5 @@ export class CodeBaseList extends React.Component<codebaseListProps> {
 }
 
 type codebaseListProps = {
-  list: Array<Object>;
+  list: Array<any>;
 };
--- frontend/public/extend/devconsole/pages/Codebases.tsx
+++ frontend/public/extend/devconsole/pages/Codebases.tsx
@@ -7,20 +7,46 @@ import { ColHead, ListHeader } from '../../../components/factory';
 class CodebasesPage extends React.Component<codebaseProps,codebaseState> {
   constructor(props) {
     super(props);
-  }
-  componentDidUpdate(PrevProps){
     this.updateList();
   }
+  mockList = [{ name:'Christian', namespace:"kube-dns", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' }, 
+              { name:'Nimisha', namespace:"kube-proxy", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Joshua', namespace:"kube-proxy", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Steve', namespace:"kube-dns", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Adi', namespace:"kube-proxy", description: "lorem ipsum lorem lorem ipsum lorem ipsumipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Dholiak', namespace:"kube-proxy", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Rohit', namespace:"myproject", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Sahil', namespace:"kube-proxy", description: "lorem ipsum lorem ipsum lorem lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Vikram', namespace:"kube-proxy", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Divyanshi', namespace:"myproject", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Karthik', namespace:"openshift", description: "lorem ipsum lorem ipsum llorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum orem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Debsmita', namespace:"openshift", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Baiju', namespace:"openshift", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Ibrahim', namespace:"openshift", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Harpreet', namespace:"kube-dns", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum  lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Serena', namespace:"kube-proxy", description: "lorem ipsum lorem ipsum lorem lorem ipsum lorem ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Shoubhik', namespace:"kube-proxy", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Saravana', namespace:"myproject", description: "lorem ipsum lorem ipsum lorem lorem ipsum lorem ipsum lorem ipsum lorem ipsumipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Anji', namespace:"kube-proxy", description: "lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum lorem ipsum lorem ipsum  ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Vidhya', namespace:"kube-proxy", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+              { name:'Karuna', namespace:"kube-proxy", description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum", location:'https://www.redhat.com' },
+            ];
   state ={
     displayList:[],
   }
+  namespace = this.props.namespace;  
+  ns = this.namespace?this.namespace:'all projects';
+  headingTitle = "View Codebases in " + this.ns;
   updateList(){
-    this.state.displayList = [ { name:'apple' }, { name:'mango' }];
+    if(this.ns == 'all projects'){
+      this.state.displayList = this.mockList;
+    }
+    else{
+      this.state.displayList = this.mockList.filter(item => item.namespace == this.ns);
+    }
   }
   render() {
-    const { namespace } = this.props;  
-    const ns = namespace?namespace:'all projects';
-    const headingTitle = "View Codebases in " + ns;
+
 
     return(
       <React.Fragment>
@@ -28,14 +54,13 @@ class CodebasesPage extends React.Component<codebaseProps,codebaseState> {
         <title>Codebases</title>
       </Helmet>
       <div className="codeBaseContainer">
-        <h2 className="codeBaseMainTitle"> {headingTitle} </h2>
+        <h2 className="codeBaseMainTitle"> {this.headingTitle} </h2>
         <div className="codeBaseHeader">
           <ListHeader>
-            <ColHead className="col-lg-3 col-md-3 col-sm-4 col-xs-6" sortField="metadata.name">Codebase</ColHead>
-            <ColHead className="col-lg-2 col-md-3 col-sm-4 col-xs-6" sortField="metadata.namespace">Namespace</ColHead>
-            <ColHead className="col-lg-3 col-md-3 col-sm-4 hidden-xs" sortField="metadata.labels">Description</ColHead>
-            <ColHead className="col-lg-2 col-md-3 hidden-sm hidden-xs" sortField="spec.selector">Type</ColHead>
-            <ColHead className="col-lg-2 hidden-md hidden-sm hidden-xs" sortField="spec.clusterIP">Location</ColHead>
+            <ColHead className="col-lg-2 col-md-2 col-sm-4 col-xs-6" sortField="metadata.name">Codebase</ColHead>
+            <ColHead className="col-lg-2 col-md-2 col-sm-4 col-xs-6" sortField="metadata.namespace">Namespace</ColHead>
+            <ColHead className="col-lg-5 col-md-5 col-sm-4 hidden-xs" sortField="metadata.labels">Description</ColHead>
+            <ColHead className="col-lg-3 col-md-3 hidden-sm hidden-xs" sortField="spec.selector">Location</ColHead>
           </ListHeader>
           <CodeBaseList list={this.state.displayList}></CodeBaseList>
         </div>
