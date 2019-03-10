5bf5913ff5477135502e61c0109908b8d14709e0
root <root@localhost.localdomain>
2019-03-10 16:40:26 +0530

    feat(codebases): separate out components in different file and use tsx code

--- frontend/public/extend/devconsole/components/codebases/CodebaseList.tsx
+++ frontend/public/extend/devconsole/components/codebases/CodebaseList.tsx
@@ -1,22 +1,11 @@
 import * as React from 'react';
-import CodebaseDetails from './CodebaseDetails'
 import CodebaseHeader from './CodebaseHeader'
 import CodebaseRow from './CodebaseRow'
-import { DetailsPage, List, ListPage } from '../../../../components/factory';
-import { Kebab, navFactory } from '../../../../components/utils';
-
-const menuActions = [Kebab.factory.ModifyPodSelector, ...Kebab.factory.common];
-
-
-const {details, pods, editYaml} = navFactory;
-const CodebaseDetailsPage = props => <DetailsPage
-  {...props}
-  menuActions={menuActions}
-  namespace = {props.namespace} 
-  pages={[details(CodebaseDetails), editYaml(), pods()]}
-/>;
+import { List } from '../../../../components/factory';
 
 const CodebaseList = props => <List {...props} Header={CodebaseHeader} Row={CodebaseRow} />;
-const CodebasePage = props => <ListPage canCreate={false} ListComponent={CodebaseList} {...props} />;
 
-export {CodebaseList, CodebasePage, CodebaseDetailsPage};
+export default CodebaseList;
+
+
+
