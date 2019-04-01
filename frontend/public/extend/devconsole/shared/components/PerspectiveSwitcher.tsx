83226420023a68f6c88e388fbe34866827d558e1
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2019-04-01 19:14:29 +0530

    Created mega-menu nav comp and added it in persepective switcher comp

--- frontend/public/extend/devconsole/shared/components/PerspectiveSwitcher.tsx
+++ frontend/public/extend/devconsole/shared/components/PerspectiveSwitcher.tsx
@@ -1,48 +1,28 @@
 /* eslint-disable no-unused-vars, no-undef */
 import * as React from 'react';
-import { Modal } from '@patternfly/react-core';
-import { NavLink } from 'react-router-dom';
-import './PerspectiveSwitcher.scss';
 import * as openshiftIconImg from '../../../../imgs/openshift-favicon.png';
+import MegaMenu from '../../shared/components/MegaMenu/MegaMenu';
+import MegaMenuItem from './MegaMenu/MegaMenuItem';
+import MegaMenuSection from './MegaMenu/MegaMenuSection';
+import MegaMenuNav from './MegaMenu/MegaMenuNav';
 
 export interface PerspectiveSwitcherProps {
   isNavOpen: boolean,
   onNavToggle: (MouseEvent) => void,
 }
 
-const PerspectiveSwitcher: React.SFC<PerspectiveSwitcherProps> = (props: PerspectiveSwitcherProps) => (
-  <Modal
-    isLarge
-    title=""
-    isOpen={props.isNavOpen}
-    onClose={props.onNavToggle}
-    className="devops-perspective-switcher">
-    <nav className="pf-c-nav">
-      <ul className="pf-c-nav__simple-list">
-        <li className="pf-c-nav__item">
-          <NavLink
-            to="/devops"
-            onClick={props.onNavToggle}
-            className="pf-c-nav__link"
-            activeClassName="pf-m-current"
-          >
-          <img src={openshiftIconImg} alt="DevOps Console" className="devops-perspective-switcher__openshift-logo"/>  DevOps Console
-          </NavLink>
-        </li>
-        <li className="pf-c-nav__item">
-          <NavLink
-            to="/k8s/cluster/projects"
-            onClick={props.onNavToggle}
-            className="pf-c-nav__link"
-            isActive={(match, { pathname }):boolean => !pathname.startsWith('/devops')}
-            activeClassName="pf-m-current"
-          >
-            <img src={openshiftIconImg} alt="Admin Console" className="devops-perspective-switcher__openshift-logo"/> Admin Console
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </Modal>
-);
+function PerspectiveSwitcher(props: PerspectiveSwitcherProps) {
+  return (
+    <MegaMenu isNavOpen={props.isNavOpen}>
+      <MegaMenuNav onSelect={props.onNavToggle}>
+        <MegaMenuSection title="Example">
+          <MegaMenuItem to="https://cloud.openshift.com" title="Multi-Cluster Manager" icon={openshiftIconImg} externalLink/>
+          <MegaMenuItem to="/devops" title="Dev Console" icon={openshiftIconImg}/>
+          <MegaMenuItem to="/k8s/cluster/projects" title="Admin Console" icon={openshiftIconImg}/>
+        </MegaMenuSection>
+      </MegaMenuNav>
+    </MegaMenu>
+  );
+}
 
 export default PerspectiveSwitcher;
