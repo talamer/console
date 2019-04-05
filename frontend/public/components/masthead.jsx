745b40c8c0502b55332b2160906991246a1d8879
debsmita1 <debsmita.santra@gmail.com>
2019-04-05 15:37:05 +0530

    Updated styles of the Nav Toggle icon

--- frontend/public/components/masthead.jsx
+++ frontend/public/components/masthead.jsx
@@ -1,49 +1,49 @@
-import * as React from 'react';
-import * as PropTypes from 'prop-types';
-import { Brand, PageHeader } from '@patternfly/react-core';
+import * as React from "react";
+import * as PropTypes from "prop-types";
+import { Brand, PageHeader } from "@patternfly/react-core";
 
-import { MastheadToolbar } from './masthead-toolbar';
-import { history } from './utils';
-import okdLogoImg from '../imgs/okd-logo.svg';
-import openshiftLogoImg from '../imgs/openshift-logo.svg';
-import ocpLogoImg from '../imgs/openshift-platform-logo.svg';
-import onlineLogoImg from '../imgs/openshift-online-logo.svg';
-import dedicatedLogoImg from '../imgs/openshift-dedicated-logo.svg';
-import azureLogoImg from '../imgs/azure-red-hat-openshift-logo.svg';
+import { MastheadToolbar } from "./masthead-toolbar";
+import { history } from "./utils";
+import okdLogoImg from "../imgs/okd-logo.svg";
+import openshiftLogoImg from "../imgs/openshift-logo.svg";
+import ocpLogoImg from "../imgs/openshift-platform-logo.svg";
+import onlineLogoImg from "../imgs/openshift-online-logo.svg";
+import dedicatedLogoImg from "../imgs/openshift-dedicated-logo.svg";
+import azureLogoImg from "../imgs/azure-red-hat-openshift-logo.svg";
 
 export const getBrandingDetails = () => {
   let logoImg, logoAlt, productTitle;
   // Webpack won't bundle these images if we don't directly reference them, hence the switch
   switch (window.SERVER_FLAGS.branding) {
-    case 'openshift':
+    case "openshift":
       logoImg = openshiftLogoImg;
-      logoAlt = 'OpenShift';
-      productTitle = 'Red Hat OpenShift';
+      logoAlt = "OpenShift";
+      productTitle = "Red Hat OpenShift";
       break;
-    case 'ocp':
+    case "ocp":
       logoImg = ocpLogoImg;
-      logoAlt = 'OpenShift Container Platform';
-      productTitle = 'Red Hat OpenShift Container Platform';
+      logoAlt = "OpenShift Container Platform";
+      productTitle = "Red Hat OpenShift Container Platform";
       break;
-    case 'online':
+    case "online":
       logoImg = onlineLogoImg;
-      logoAlt = 'OpenShift Online';
-      productTitle = 'Red Hat OpenShift Online';
+      logoAlt = "OpenShift Online";
+      productTitle = "Red Hat OpenShift Online";
       break;
-    case 'dedicated':
+    case "dedicated":
       logoImg = dedicatedLogoImg;
-      logoAlt = 'OpenShift Dedicated';
-      productTitle = 'Red Hat OpenShift Dedicated';
+      logoAlt = "OpenShift Dedicated";
+      productTitle = "Red Hat OpenShift Dedicated";
       break;
-    case 'azure':
+    case "azure":
       logoImg = azureLogoImg;
-      logoAlt = 'Azure Red Hat OpenShift';
-      productTitle = 'Azure Red Hat OpenShift';
+      logoAlt = "Azure Red Hat OpenShift";
+      productTitle = "Azure Red Hat OpenShift";
       break;
     default:
       logoImg = okdLogoImg;
-      logoAlt = 'OKD';
-      productTitle = 'OKD';
+      logoAlt = "OKD";
+      productTitle = "OKD";
   }
   return { logoImg, logoAlt, productTitle };
 };
@@ -67,14 +67,15 @@ export const Masthead = ({
   return (
     <PageHeader
       className={[
-        isPerspectiveSwitcherActive ? 'nav-toggle__hover' : '',
-        isPerspectiveSwitcherActive && isNavOpen ? 'nav-toggle__active' : ''
+        isPerspectiveSwitcherActive ? "odc-mega-menu-toggle__hover" : "",
+        isPerspectiveSwitcherActive && isNavOpen
+          ? "odc-mega-menu-toggle__active"
+          : ""
       ]}
       logo={<Brand src={details.logoImg} alt={details.logoAlt} />}
       logoProps={logoProps}
       toolbar={<MastheadToolbar />}
       showNavToggle
-      isNavOpen={isNavOpen}
       onNavToggle={onNavToggle}
     />
   );
