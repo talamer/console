2b49456426ef7065c249e8c201345c4212a0176c
root <root@localhost.localdomain>
2019-04-25 16:08:56 +0530

    feat(pipeline):Add to resource page

--- frontend/public/components/resource-pages.ts
+++ frontend/public/components/resource-pages.ts
@@ -94,7 +94,7 @@ export const resourceDetailPages = ImmutableMap<GroupVersionKind | string, () =>
   .set(referenceForModel(ServiceInstanceModel), () => import('./service-instance' /* webpackChunkName: "service-instance" */).then(m => m.ServiceInstanceDetailsPage))
   .set(referenceForModel(ServiceBindingModel), () => import('./service-binding' /* webpackChunkName: "service-binding" */).then(m => m.ServiceBindingDetailsPage))
   .set(referenceForModel(ServiceModel), () => import('./service' /* webpackChunkName: "service" */).then(m => m.ServicesDetailsPage))
-  .set(referenceForModel(PipelineModel), () => import('../extend/devconsole/components/pipelines/pipeline-details-page' /* webpackChunkName: "PipelineDetailsPage" */).then(m => m.default))
+  .set(referenceForModel(PipelineModel), () => import('../extend/devconsole/components/pipelines/PipelineDetailsPage' /* webpackChunkName: "PipelineDetailsPage" */).then(m => m.default))
   .set(referenceForModel(IngressModel), () => import('./ingress' /* webpackChunkName: "ingress" */).then(m => m.IngressesDetailsPage))
   .set(referenceForModel(RouteModel), () => import('./routes' /* webpackChunkName: "routes" */).then(m => m.RoutesDetailsPage))
   .set(referenceForModel(ClusterRoleModel), () => import('./RBAC/role' /* webpackChunkName: "role" */).then(m => m.ClusterRolesDetailsPage))
