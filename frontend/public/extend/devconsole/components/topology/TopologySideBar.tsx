c6b1dfba6bb7f615080fb137490d02a2b144da90
sahil143 <sahilbudhwar143@gmail.com>
2019-04-29 12:52:38 +0530

    reduce the height of sidebar

--- frontend/public/extend/devconsole/components/topology/TopologySideBar.tsx
+++ frontend/public/extend/devconsole/components/topology/TopologySideBar.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable no-unused-vars, no-undef */
 import * as React from 'react';
 import { TopologyDataObject } from './topology-types';
 import { ModelessOverlay } from 'patternfly-react';
 import { CloseButton } from '../../../../components/utils';
 import { ResourceOverviewPage } from '../../../../components/overview/resource-overview-page';
+import './TopologySideBar.scss'
 
-type TopologySideBarProps = {
+export type TopologySideBarProps = {
   item: TopologyDataObject;
   selected: string;
   onClose: Function;
@@ -33,9 +35,9 @@ class TopologySideBar extends React.Component<TopologySideBarProps> {
     const ItemtoShowOnSideBar = this.getSelectedItemObj(this.props.item);
     return (
       <>
-      <ModelessOverlay show={!!this.props.selected}>
-      <div className="overview__sidebar-dismiss clearfix">
-            <CloseButton onClick={this.props.onClose} />
+      <ModelessOverlay className="odc-topology-sidebar__overlay" show={!!this.props.selected}>
+        <div className="odc-topology-sidebar__dismiss clearfix">
+            <CloseButton onClick={this.props.onClose} data-test-id='sidebar-close-button'/>
           </div>
           <ResourceOverviewPage
             item={ItemtoShowOnSideBar}
