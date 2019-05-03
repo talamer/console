d4f11a098c8a88ee3d7c70bd70b16fa6b1e895cb
root <root@localhost.localdomain>
2019-05-03 13:59:46 +0530

    fix(pipeline) Change label selector

--- frontend/public/extend/devconsole/components/pipelines/PipelineRuns.tsx
+++ frontend/public/extend/devconsole/components/pipelines/PipelineRuns.tsx
@@ -24,7 +24,7 @@ interface PipelineRunsProps {
 class PipelineRuns extends React.Component<PipelineRunsProps> {
   render() {
     const selector = {
-      "tekton.dev/pipeline": this.props.obj.metadata.name,
+      'tekton.dev/pipeline': this.props.obj.metadata.name,
     };
     return (
       <ListPage
