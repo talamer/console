f68d0cc22415c78016fee6c503b0d1659948f0dd
Rohit Rai <rohitkrai03@gmail.com>
2019-05-14 18:42:26 +0530

    Fixes wrong error message when no namespace is not selected

--- frontend/public/extend/devconsole/components/ImportFlowForm/ImportFlowForm.tsx
+++ frontend/public/extend/devconsole/components/ImportFlowForm/ImportFlowForm.tsx
@@ -72,6 +72,7 @@ enum ErrorMessage {
   BuilderImageError = 'We failed to detect the builder image. Please select an appropriate image.',
   GitUrlError = 'Please enter a valid git URL.',
   GitTypeError = 'We failed to detect the git type. Please choose a git type.',
+  NamespaceNotSelected = 'Please select a Namespace to continue.',
 }
 
 const getUrlErrorMessage = (errorType: ErrorMessage) =>
@@ -180,7 +181,13 @@ export class ImportFlowForm extends React.Component<Props, State> {
   };
 
   handleNamespaceChange = (namespace: string) => {
-    this.setState({ namespace });
+    this.setState(({ gitRepoUrlError }) => {
+      const error = gitRepoUrlError !== ErrorMessage.NamespaceNotSelected ? gitRepoUrlError : '';
+      return {
+        namespace,
+        gitRepoUrlError: error,
+      };
+    }, this.validateGitRepo);
   };
 
   onApplicationChange = (application: string, selectedKey: string) => {
@@ -240,6 +247,10 @@ export class ImportFlowForm extends React.Component<Props, State> {
   }
 
   validateGitRepo = (): void => {
+    if (!this.state.namespace) {
+      this.setState({ gitRepoUrlError: ErrorMessage.NamespaceNotSelected });
+      return;
+    }
     if (
       this.state.lastEnteredGitUrl !== this.state.gitRepoUrl &&
       this.state.gitRepoUrlError === ''
