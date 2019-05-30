c9777b5d7dfadaa5b5de7a42a32c7b7e6f5a8369
sahil143 <sahilbudhwar143@gmail.com>
2019-05-30 10:00:29 +0530

    change the props for base edge, move marker defs to ConnectsTo component

--- frontend/public/extend/devconsole/components/topology/shapes/BaseEdge.tsx
+++ frontend/public/extend/devconsole/components/topology/shapes/BaseEdge.tsx
@@ -1,32 +1,25 @@
 import * as React from 'react';
 import './BaseEdge.scss';
-import SvgArrowMarker from './../../../shared/components/svg/SvgArrowMarker';
 import { createSvgIdUrl } from '../../../shared/utils/svg-utils';
+import { ViewNode } from '../topology-types';
 
 type BaseEdgeProps = {
-  source: any;
-  target: any;
-  sourceArrow?: boolean;
-  targetArrow?: boolean;
+  source: ViewNode;
+  target: ViewNode;
+  sourceMarkerId?: string;
+  targetMarkerId?: string;
 };
 
-const SOURCE_ARROW_MARKER_ID = 'sourceArrowMarker';
-const TARGET_ARROW_MARKER_ID = 'targetArrowMarker';
-
-const BaseEdge: React.SFC<BaseEdgeProps> = ({ source, target, sourceArrow = false, targetArrow = false }) => (
-  <g>
-    {sourceArrow ? <SvgArrowMarker id={SOURCE_ARROW_MARKER_ID}  /> : null}
-    {targetArrow ? <SvgArrowMarker id={TARGET_ARROW_MARKER_ID}  /> : null}
+const BaseEdge: React.SFC<BaseEdgeProps> = ({ source, target, sourceMarkerId, targetMarkerId }) => (
     <line
       className="odc-base-edge"
       x1={source.x}
       y1={source.y}
       x2={target.x}
       y2={target.y}
-      markerStart={sourceArrow ? createSvgIdUrl(SOURCE_ARROW_MARKER_ID) : undefined}
-      markerEnd={targetArrow ? createSvgIdUrl(TARGET_ARROW_MARKER_ID) : undefined}
+      markerStart={sourceMarkerId ? createSvgIdUrl(sourceMarkerId) : undefined}
+      markerEnd={targetMarkerId ? createSvgIdUrl(targetMarkerId) : undefined}
     />
-  </g>
 );
 
 export default BaseEdge;
--- frontend/public/extend/devconsole/components/topology/shapes/ConnectsTo.tsx
+++ frontend/public/extend/devconsole/components/topology/shapes/ConnectsTo.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import { EdgeProps } from '../topology-types';
 import BaseEdge from './BaseEdge';
+import SvgArrowMarker from 'public/extend/devconsole/shared/components/svg/SvgArrowMarker';
 
 type ConnectsToProps = EdgeProps;
 
+const TARGET_ARROW_MARKER_ID = 'connectsToTargetArrowMarker';
+
 const ConnectsTo: React.FC<ConnectsToProps> = ({ source, target }) => (
-  <BaseEdge source={source} target={target} targetArrow={true} sourceArrow={false} />
+  <React.Fragment>
+    <SvgArrowMarker id={TARGET_ARROW_MARKER_ID}  />
+    <BaseEdge source={source} target={target} targetMarkerId={TARGET_ARROW_MARKER_ID} />
+  </React.Fragment>
 );
 
 export default ConnectsTo;
