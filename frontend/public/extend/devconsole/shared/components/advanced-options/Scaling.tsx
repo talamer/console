caf4cef9c170e09500aaf281895f9d1bcec82eda
debsmita1 <debsmita.santra@gmail.com>
2019-06-04 13:35:25 +0530

    address review comments

--- frontend/public/extend/devconsole/shared/components/advanced-options/Scaling.tsx
+++ frontend/public/extend/devconsole/shared/components/advanced-options/Scaling.tsx
@@ -1,46 +1,46 @@
+/* eslint-disable no-undef, dot-notation */
 import * as React from 'react';
 import * as _ from 'lodash-es';
 import { FormGroup, ControlLabel, HelpBlock } from 'patternfly-react';
-import './Scaling.scss';
 import { NumberSpinner } from '../../../../../components/utils';
 
 interface ScalingProps {
-  replicas: number;
+  replicas?: number;
+  replicaError?: string;
   onChange: (replicas: number) => void;
 }
 
-export default class Scaling extends React.PureComponent<ScalingProps> {
-  onChangeData: React.ReactEventHandler<HTMLInputElement> = (event) => {
-    this.props.onChange(event.currentTarget.valueAsNumber);
+export const isPositiveInteger = (replicas: number) => {
+  return _.isInteger(replicas) && replicas >= 0
+    ? ''
+    : 'Replicas must be an integer value greater than or equal to 0.';
+};
+
+const Scaling: React.FC<ScalingProps> = ({ replicas, replicaError, onChange }) => {
+  const onChangeData: React.ReactEventHandler<HTMLInputElement> = (event) => {
+    onChange(event.currentTarget.valueAsNumber);
   };
 
-  changeValueBy = (operation) => {
-    this.props.onChange(_.toInteger(this.props.replicas) + operation);
-  };
+  return (
+    <React.Fragment>
+      <div className="co-section-heading">Scaling</div>
+      <div className="co-section-heading-tertiary">
+        Replicas are scaled manually based on CPU usage.
+      </div>
+      <FormGroup controlId="scaling-replicas" className={replicaError ? 'has-error' : ''}>
+        <ControlLabel>Replicas</ControlLabel>
+        <NumberSpinner
+          id="scaling-replicas"
+          className="form-control"
+          value={replicas}
+          onChange={onChangeData}
+          changeValueBy={(operation: number) => onChange(_.toInteger(replicas) + operation)}
+        />
+        <HelpBlock>The number of instances of your image.</HelpBlock>
+        <HelpBlock>{replicaError}</HelpBlock>
+      </FormGroup>
+    </React.Fragment>
+  );
+};
 
-  isPositiveInteger = () => {
-    return _.isInteger(this.props.replicas) && this.props.replicas >= 0;
-  };
-
-  render() {
-    return (
-      <React.Fragment>
-        <h2 className="odc-scaling">Scaling</h2>
-        <h4 className="odc-scaling-info">Replicas are scaled manually based on CPU usage. </h4>
-        <FormGroup className={this.isPositiveInteger() ? '' : 'has-error'}>
-          <ControlLabel>Replicas</ControlLabel>
-          <NumberSpinner
-            className="form-control"
-            value={this.props.replicas}
-            onChange={this.onChangeData}
-            changeValueBy={this.changeValueBy}
-          />
-          <HelpBlock>The number of instances of your image.</HelpBlock>
-          {!this.isPositiveInteger() && (
-            <HelpBlock>Replicas must be an integer value greater than or equal to 0.</HelpBlock>
-          )}
-        </FormGroup>
-      </React.Fragment>
-    );
-  }
-}
+export default Scaling;
