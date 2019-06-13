b79dfd204ef4e94745c55fb79255bb3d1ebd5422
Karthik <karthik.jk11@gmail.com>
2019-06-13 12:52:23 +0530

    fix pipeline utils test cases

--- frontend/public/extend/devconsole/shared/utils/__tests__/pipeline-utils.spec.ts
+++ frontend/public/extend/devconsole/shared/utils/__tests__/pipeline-utils.spec.ts
@@ -5,16 +5,16 @@ import { mockPipelinesJSON } from '../__mocks__/PipelineUtilMocks';
 describe('pipeline-utils ', () => {
   it('For first pipeline there should be 2 stages of length [0:[1],1:[2]]', () => {
     const stages = getPipelineTasks(mockPipelinesJSON[0]);
-    expect(stages.length).toBe(2);
-    expect(stages[0].length).toBe(1);
-    expect(stages[1].length).toBe(1);
+    expect(stages).toHaveLength(2);
+    expect(stages[0]).toHaveLength(1);
+    expect(stages[1]).toHaveLength(1);
   });
   it('should transform pipelines', () => {
     const stages = getPipelineTasks(mockPipelinesJSON[1]);
-    expect(stages.length).toBe(4);
-    expect(stages[0].length).toBe(1);
-    expect(stages[1].length).toBe(2);
-    expect(stages[2].length).toBe(2);
-    expect(stages[3].length).toBe(1);
+    expect(stages).toHaveLength(4);
+    expect(stages[0]).toHaveLength(1);
+    expect(stages[1]).toHaveLength(2);
+    expect(stages[2]).toHaveLength(2);
+    expect(stages[3]).toHaveLength(1);
   });
 });
