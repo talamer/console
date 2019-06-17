d649b779f38a6d20620400ac360ecacd7dd841bc
Karthik <karthik.jk11@gmail.com>
2019-06-17 23:56:11 +0530

    fix pipeline delay

--- frontend/public/extend/devconsole/shared/yamls/pipelines/install_pipeline_mocks.sh
+++ frontend/public/extend/devconsole/shared/yamls/pipelines/install_pipeline_mocks.sh
@@ -13,15 +13,15 @@ sleep 10
 oc create -f resources.yaml
 echo -e "\e[100m \e[94m==   -  STEP 3/5 -   == \e[97m \e[49m"
 echo -e "\e[32m - CREATING TASKS  - \e[97m"
-sleep 1
+sleep 10
 oc create -f task.yaml
 echo -e "\e[100m \e[94m==   -  STEP 4/5 -   == \e[97m \e[49m"
 echo -e "\e[32m  -  CREATING PIPELINES  -  \e[97m"
-sleep 1
+sleep 10
 oc create -f pipeline.yaml
 echo -e "\e[100m \e[94m==   -  STEP 5/5 -   == \e[97m \e[49m"
 echo -e "\e[32m -  CREATING PIPELINERUNS!! THIS SHOULD TAKE 9-13 SECONDS  - \e[97m"
-sleep 8
+sleep 10
 oc create -f pipelinerun.yaml
 echo -e "\e[100m \e[94m   DONE !! ENJOY YOUR DAY !! DEV CONSOLE UI  \e[97m \e[49m"
 echo -e "\e[100m \e[94m ======================================== \e[97m \e[49m \e[0m"
